42350fb6cb6a7c51024c2a72e640494ebed200e8
Ilja Malakhov <malakhov.i@milandr.ru>
2020-12-28 18:57:08 +0300

    Initial Commit

---
+++ ohMyGithubSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ohMyGithubSearch
+//
+//  Created by Milandr on 28.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
